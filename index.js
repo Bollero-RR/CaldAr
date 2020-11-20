c68abbaea3a15ef03318a2c5c73988620f924e5a
DavidArroyo1992 <davidstream1992@gmail.com>
2020-11-20 13:00:08 -0300

    create express server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(‘hi’)
+//console.log(‘hi’)
+
+const express = require('express');
+
+//INIT EXPRESS
+
+const app = express();
+
+//CREATE ROUTE
+
+app.get('/', (req,res)=>{
+    res.send('hello')
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
