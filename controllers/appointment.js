410bb7b91c8edac6d0339476765e9d207e36795b
IngAgustinAlmada <agustin.poli2012@gmail.com>
2020-11-22 13:34:44 -0300

    Edit appointment controller

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -1,16 +1,38 @@
-//ES5
-function getAllAppointments(){
-    console.log("This is not a test")
-}
-//ES6
-const getAllAppointments = () => console.log("This is not a test");
+// const getAllAppointments = () => console.log("This is not a test");
+// const getAppointmentById = () => console.log("This is not a test");
+// const getAppointmentByAttribute = () => console.log("This is not a test");
+// const deleteAppointmentById = () => console.log("This is not a test");
 
-function getAppointmentById(){
+const express = require("express");
+const router = express.Router();
+const appointments = require("../data/appointment.json");
 
-}
-function getAppointmentByAttribute(){
+const idFilter = (req) => (appointment) => appointment.id === parseInt(req.params.id);
 
-}
-function deleteAppointmentById(){
+//Get all Appointments
+router.get("/", (req, res) => res.json(appointments));
 
-}
+//Get all single appointment
+router.get("/:id", (req, res) => {
+  const found = appointments.some(idFilter(req));
+  if (found) {
+    res.json(appointments.filter(idFilter(req)));
+  } else {
+    res.status(400).json({ msg: `No appointment with the id of ${req.params.id}` });
+  }
+});
+
+//Delete appointment
+router.delete("/:id", (req, res) => {
+  const found = appointments.some(idFilter(req));
+  if (found) {
+    res.json({
+      msg: "appointment deleted",
+      appointments: appointments.filter((appointment) => !idFilter(req)(appointment)),
+    });
+  } else {
+    res.status(400).json({ msg: `No appointment with the id of ${req.params.id}` });
+  }
+});
+
+module.exports = router;
