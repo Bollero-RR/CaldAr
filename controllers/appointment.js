9588bd247a46ef93eef0fc3df9665daf0240e457
IngAgustinAlmada <agustin.poli2012@gmail.com>
2020-11-22 13:08:01 -0300

    add constrollers appointments

---
+++ controllers/appointment.js
@@ -0,0 +1,16 @@
+//ES5
+function getAllAppointments(){
+    console.log("This is not a test")
+}
+//ES6
+const getAllAppointments = () => console.log("This is not a test");
+
+function getAppointmentById(){
+
+}
+function getAppointmentByAttribute(){
+
+}
+function deleteAppointmentById(){
+
+}
