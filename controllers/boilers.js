30840b3beb43c0c382577b709afc9444e07b0c69
maxivgg <maxivgg@gmail.com>
2020-11-22 12:26:37 -0300

    edit controller boilers

--- controllers/boilers.js
+++ controllers/boilers.js
@@ -1,12 +1,35 @@
-function getAllBoilers(){
+const express = require("express");
+const router = express.Router();
+const boilers = require("../data/boilers.json");
 
-}
-function getBoilerById(){
+const idFilter = (req) => (boiler) => boiler.id === parseInt(req.params.id);
 
-}
-function getBoilersByAttribute(){
+//Get all boilers
+router.get("/", (req, res) => res.json(boilers));
 
-}
-function deleteBoilerById(){
+//Get all single boiler
+router.get("/:id", (req, res) => {
+  const found = boilers.some(idFilter(req));
 
-}
+  if (found) {
+    res.json(boilers.filter(idFilter(req)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+//Delete boiler
+router.delete("/:id", (req, res) => {
+  const found = boilers.some(idFilter(req));
+
+  if (found) {
+    res.json({
+      msg: "Member deleted",
+      boilers: boilers.filter((boiler) => !idFilter(req)(boiler)),
+    });
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const app = express();
 app.get('/', (req,res)=>{
     res.send('hello')
 });
+
+// Members API Routes
+app.use('/boilers', require('./controllers/boilers'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
