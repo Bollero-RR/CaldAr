7b60f842131617d6bdd7971068ba5f0a13f4083a
IngAgustinAlmada <agustin.poli2012@gmail.com>
2020-11-22 13:13:53 -0300

    add Appointment API Routes

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const app = express();
 app.get('/', (req,res)=>{
     res.send('hello')
 });
+
+// Appointment API Routes
+app.use('/appointment', require('./controllers/appointment.js'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
