3e00be2eb12c367840f3f071a6c548483b6f3f5f
DavidArroyo1992 <davidstream1992@gmail.com>
2020-11-26 09:32:33 -0300

    add building routes

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const express = require('express');
 
 const app = express();
 
-//CREATE ROUTE
+//BUILDING API ROUTES
+app.use('/Buildings', require('./controllers/buildings'));
 
-app.get('/', (req,res)=>{
-    res.send('hello')
-});
+//CREATE ROUTE
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
