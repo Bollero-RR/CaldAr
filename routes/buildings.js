becc9265ca4cdc28e0903abb26eaf8f55d503ac8
DavidArroyo1992 <davidstream1992@gmail.com>
2020-11-30 10:26:47 -0300

    add phone routes

--- routes/buildings.js
+++ routes/buildings.js
@@ -11,6 +11,9 @@ router.post("/", building.create);
 //retriev a single building with id
 router.get("/:id", building.findOne);
 
+//retrieve a single building by phone
+router.get("/phone/:phone", building.findOnePhone);
+
 //update a building with id
 router.put("/:id", building.update);
 
