c12584b8c4a407b339149aaf3f0405d89dade0ba
IngAgustinAlmada <agustin.poli2012@gmail.com>
2020-12-01 18:28:34 -0300

    Add new routes for explore the db

--- models/index.js
+++ models/index.js
@@ -4,5 +4,6 @@ const db = {}
 db.mongoose = mongoose;
 db.url = 'mongodb+srv://admin:MqQs80dXetvVdWo4@cluster0.58ub4.mongodb.net/caldar?retryWrites=true&w=majority';
 db.appointment = require("./appointment")(mongoose);
-
+//db.boilerType = require('./boilerType')(mongoose);
+db.technician = require('./technician')(mongoose);
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
-const { route } = require('./appointment');//este no es necesario creo.
-const appointmentRouter = require("./appointment")
+const appointmentRouter = require("./appointment");
+//const boilerTypeRouter = require("./boilerType");
+const technicianRouter = require("./technician");
 
-var router = require("express").Router();
+const router = require("express").Router();
 
 router.use("/appointment", appointmentRouter)
+//router.use("/boilerType", boilerTypeRouter)
+router.use("/technicians", technicianRouter)
+
 
 module.exports = router;
