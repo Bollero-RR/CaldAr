7896175fd276e023c606a4d6c7a356d58bafdae8
maxivgg <maxivgg@gmail.com>
2020-12-28 17:02:39 -0300

    make it better

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const db = {};
 db.mongoose = mongoose;
 
-db.url = process.env.DB_URL;
+//db.url = process.env.DB_URL;
+db.url = "mongodb+srv://admin:radium2120@cluster0.58ub4.mongodb.net/caldar?retryWrites=true&w=majority"
 
 db.boilerType = require("./boilerType")(mongoose);
 db.customer = require("./customer")(mongoose);
