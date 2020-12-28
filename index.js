03ef3014231bd0b2390c189d6bef3509f9f63cc9
maxivgg <maxivgg@gmail.com>
2020-12-28 15:57:34 -0300

    add cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const db = require("./models");
 const router = require("./routes");
+const cors = require('cors');
 
 require('dotenv').config()
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
