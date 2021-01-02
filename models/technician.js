66e61fbace09336bc234a0fe13d1da751d9dc3cc
maxivgg <maxivgg@gmail.com>
2021-01-01 23:34:04 -0300

    fived technician

--- models/technician.js
+++ models/technician.js
@@ -1,26 +1,26 @@
-module.exports = mongoose => {
-    const Technician = mongoose.model (
-        "technician",
-        mongoose.Schema(
-            {
-                firstName: String,
-                lastName: String,
-                email: String,
-                typeIds: [
-                    {
-                        type: mongoose.Schema.Types.ObjectId
-                    }
-                ],
-                skillsId: [
-                    {
-                        type: mongoose.Schema.Types.ObjectId
-                    }
-                ],
-                hour_rate: String,
-                daily_capacity: Number
-            },
-            { timestamps: true }
-        )
+module.exports = (mongoose) => {
+  const Technician = mongoose.model(
+    "technician",
+    mongoose.Schema(
+      {
+        firstName: String,
+        lastName: String,
+        email: String,
+        typeIds: [
+          {
+            type: mongoose.Schema.Types.ObjectId,
+          },
+        ],
+        skillsId: [
+          {
+            type: mongoose.Schema.Types.ObjectId,
+          },
+        ],
+        hour_rate: String,
+        daily_capacity: Number,
+      },
+      { timestamps: true }
     )
-    return Technician
+  );
+  return Technician;
 };
