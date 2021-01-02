2f50156381653e0ade8354bde7b625737b5e2a5b
Maria Sol Aranda <arandamariasol@gmail.com>
2021-01-01 21:37:12 -0300

    Some branch errors fixed

--- models/index.js
+++ models/index.js
@@ -2,19 +2,10 @@ const mongoose = require("mongoose");
 
 const db = {};
 db.mongoose = mongoose;
-<<<<<<< HEAD
 
 //db.url = process.env.DB_URL;
 db.url = "mongodb+srv://admin:radium2120@cluster0.58ub4.mongodb.net/caldar?retryWrites=true&w=majority"
 
-=======
-db.url =
-  "mongodb+srv://admin:MqQs80dXetvVdWo4@cluster0.58ub4.mongodb.net/caldar?retryWrites=true&w=majority";
-
-db.boilerType = require("./boilerType")(mongoose);
-db.customer = require("./customer")(mongoose);
-db.boilers = require("./boiler")(mongoose);
->>>>>>> 1e6608b85d30855e19a63b164afd9b7efc10525e
 db.technician = require('./technician')(mongoose);
 
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,8 @@ const customerRouter = require("./customers");
 const boilerRouter = require("./boilers");
 const technicianRouter = require('./technician');
 
-<<<<<<< HEAD
 var router = require("express").Router();
 
 router.use('./api/technician', technicianRouter);
-=======
-router.use("/boilerType", boilerTypeRouter);
-router.use("/customers", customerRouter);
-router.use("/boilers", boilerRouter);
-router.use('./technician', technicianRouter);
->>>>>>> 1e6608b85d30855e19a63b164afd9b7efc10525e
 
 module.exports = router;
