14f9b7fad355595593650e7e5151d3b1024e5be2
maxivgg <maxivgg@gmail.com>
2021-01-03 21:02:56 -0300

    redux final

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
