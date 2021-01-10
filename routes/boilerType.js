4ce86a066fd894532392cec2723262624eea41be
IngAgustinAlmada <agustin.poli2012@gmail.com>
2021-01-10 17:39:32 -0300

    Appointment not working

--- routes/boilerType.js
+++ routes/boilerType.js
@@ -1,20 +1,21 @@
 const boilerType = require("../controllers/boilerType");
 let router = require("express").Router();
-const authMiddleWare = require("../middleware/authMiddleWare");
-
-//router.get("/", boilerType.findAll);
-//router.post("/", boilerType.create);
-//router.get("/type/:type", boilerType.findOneType);
-//router.get("/:id", boilerType.findOneId);
-//router.put("/:id", boilerType.update);
-//router.delete("/:id", boilerType.delete);
+//const authMiddleWare = require("../middleware/authMiddleWare");
 
 router
-    .get("/", authMiddleWare, boilerType.findAll)
-    .post("/", authMiddleWare, boilerType.create)
-    .get("/type/:type", authMiddleWare, boilerType.findOneType)
-    .get("/:id", authMiddleWare, boilerType.findOneId)
-    .put("/:id", authMiddleWare, boilerType.update)
-    .delete("/:id", authMiddleWare, boilerType.delete);
+    .get("/", boilerType.findAll)
+    .post("/", boilerType.create)
+    .get("/type/:type", boilerType.findOneType)
+    .get("/:id", boilerType.findOneId)
+    .put("/:id", boilerType.update)
+    .delete("/:id", boilerType.delete);;
+
+//router
+    //.get("/", authMiddleWare, boilerType.findAll)
+    //.post("/", authMiddleWare, boilerType.create)
+    //.get("/type/:type", authMiddleWare, boilerType.findOneType)
+    //.get("/:id", authMiddleWare, boilerType.findOneId)
+    //.put("/:id", authMiddleWare, boilerType.update)
+    //.delete("/:id", authMiddleWare, boilerType.delete);
 
 module.exports = router;
