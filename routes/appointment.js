72db6e485643450c860be3cbf57f06cc6664da3c
IngAgustinAlmada <agustin.poli2012@gmail.com>
2021-01-11 10:14:15 -0300

    testing

--- routes/appointment.js
+++ routes/appointment.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 let appointment = require("../controllers/appointment");
 const authMiddleWare = require("../middleware/authMiddleWare");
 
-router.get("/", authMiddleWare, appointment.findAll)
-    .post("/", authMiddleWare, appointment.create)
-    .get("/:id", authMiddleWare, appointment.findOne)
-    .get("/buildingId/:buildingId", authMiddleWare, appointment.findOneBuildingId)
-    .put("/:id", authMiddleWare, appointment.update)
-    .delete("/:id", authMiddleWare, appointment.delete);
+router.get("/", !authMiddleWare, appointment.findAll)
+    .post("/", !authMiddleWare, appointment.create)
+    .get("/:id", !authMiddleWare, appointment.findOne)
+    .get("/buildingId/:buildingId", !authMiddleWare, appointment.findOneBuildingId)
+    .put("/:id", !authMiddleWare, appointment.update)
+    .delete("/:id", !authMiddleWare, appointment.delete);
 
 module.exports = router;
 
